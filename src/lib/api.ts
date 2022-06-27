4270bfda4dabae15d7fe63d4841fd5ca643289a9
mitri-dvp <mitri.dvp@gmail.com>
2022-06-27 14:47:03 -0400

    Added fetchAPI console log

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,12 +5,15 @@ export function getStrapiURL() {
 // Helper to make GET requests to Strapi
 export async function fetchAPI(path = '', params = '') {
   const url = getStrapiURL()
-  const res = await fetch(
+  const finalURL =
     url +
-      `${path}` +
-      `?key=${process.env.NEXT_PUBLIC_API_KEY}` +
-      `&sort[0]=id:asc` +
-      `&${params}`
-  )
+    `${path}` +
+    `?key=${process.env.NEXT_PUBLIC_API_KEY}` +
+    `&sort[0]=id:asc` +
+    `&${params}`
+
+  console.log('FINAL URL', finalURL)
+
+  const res = await fetch(finalURL)
   return await res.json()
 }
