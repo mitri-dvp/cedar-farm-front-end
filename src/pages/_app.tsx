bcf56de1623fb4541602f41ed9a9bbae50c907a5
mitri-dvp <mitri.dvp@gmail.com>
2022-07-02 01:18:46 -0400

    Updated SEO tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,37 @@ import Layout from '@components/Layout'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const SEO = {
+    metaTitle: 'Cedar Farm',
+    metaDescription:
+      'Black Angus Breeder, Dorper Sheep Breeder,🌱 Raised with Care and Integrity.',
+    shareImage: `${process.env.NEXT_PUBLIC_FRONT_END_URL}/logo-2.png`
+  }
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico"></link>
+        {/* META DATA */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* SEO */}
+        <link rel="icon" href="/favicon.ico" />
+        <title>Cedar Farm</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={SEO.metaTitle} />
+        <meta name="twitter:title" content={SEO.metaTitle} />
+
+        <meta name="description" content={SEO.metaDescription} />
+        <meta property="og:description" content={SEO.metaDescription} />
+        <meta name="twitter:description" content={SEO.metaDescription} />
+
+        <meta name="image" content={SEO.shareImage} />
+        <meta property="og:image" content={SEO.shareImage} />
+        <meta name="twitter:image" content={SEO.shareImage} />
+
+        <meta name="twitter:card" content="summary" />
+
+        {/* FONTS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -26,11 +53,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link
           href="https://fonts.googleapis.com/css2?family=League+Gothic&display=swap"
           rel="stylesheet"
-        />
-        <title>Cedar Farm</title>
-        <meta
-          name="description"
-          content="Black Angus Breeder, Dorper Sheep Breeder,🌱 Raised with Care and Integrity."
         />
       </Head>
       <Layout>
